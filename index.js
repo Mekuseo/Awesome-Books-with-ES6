075dbe68b7f47692e61a30e1ee0185665ad947a1
Chukwuemeka Kanu <mekuseo@gmail.com>
2022-12-07 09:03:17 +0100

    Awesome books: with ES6

--- index.js
+++ index.js
@@ -1,20 +1,55 @@
-import {contact, contactLink} from './modules/contact.js'
-import {allBooks, listLink} from './modules/bookList.js'
-import {addBook, addNew} from './modules/addBook.js'
-import {Book, StoreBook} from "./modules/newBook.js"
+/* eslint-disable max-len */
+/* eslint-disable no-underscore-dangle */
 
-// Add event listener to contact link
-contactLink.addEventListener('click', () => {
-  contact();
+import {
+  first, second, third, body0, bodyT, bodyS,
+} from './modules/pageDynamics.js';
+import { DateTime } from './modules/luxon.js';
+import UI from './modules/classUI.js';
+import Book from './modules/classBook.js';
+
+first.addEventListener('click', () => {
+  body0.style.display = 'block';
+  bodyT.style.display = 'none';
+  bodyS.style.display = 'none';
 });
 
-// Add event listener to Book Lists link
-listLink.addEventListener('click', () => {
-  allBooks();
+second.addEventListener('click', () => {
+  body0.style.display = 'none';
+  bodyT.style.display = 'block';
+  bodyS.style.display = 'none';
 });
 
-// Add event listener to Add-Book link
-addNew.addEventListener('click', () => {
-  addBook();
+third.addEventListener('click', () => {
+  body0.style.display = 'none';
+  bodyT.style.display = 'none';
+  bodyS.style.display = 'block';
 });
 
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// add books
+
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  // prevent actual submit
+  e.preventDefault();
+  // get form values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+
+  // instantiate book
+  const book = new Book(title, author);
+
+  // add book to list
+  UI.addBookToList(book);
+
+  // clear fields
+  UI.clearFields();
+});
+
+document.querySelector('#book-list').addEventListener('click', (e) => {
+  UI.deleteBook(e.target);
+});
+
+const now = DateTime.local().toLocaleString(DateTime.DATETIME_FULL);
+document.querySelector('.date').innerHTML = `${now}`;
